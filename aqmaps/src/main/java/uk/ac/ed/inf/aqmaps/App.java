39fc94be37bff7fb52a84d7e5fac5d5483fc37c9
KIliuchok <s1807869@ed.ac.uk>
2020-10-23 14:29:45 +0800

    duh

--- aqmaps/src/main/java/uk/ac/ed/inf/aqmaps/App.java
+++ aqmaps/src/main/java/uk/ac/ed/inf/aqmaps/App.java
@@ -1,19 +1,23 @@
 package uk.ac.ed.inf.aqmaps;
 
 import com.mapbox.geojson.*;
-import com.google.*;
-/**
- * Hello world!
- *
- */
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import java.lang.reflect.Type;
+
+import java.util.*;
+
 public class App 
 {
-	private static final String urlString = "localhost:80";
+	private static final String urlString = "https:/localhost:80/maps/2020/01/03/air-quality-data.json";
 	
     public static void main( String[] args )
     {
         String string = NetworkRead.readNetworkToString(urlString);
         
+        Type listType = new TypeToken<ArrayList<AirQualityEntry>>() {}.getType();
+        ArrayList<AirQualityEntry> listOfEntries = new Gson().fromJson(string, listType);
         
+        System.out.println(listOfEntries.size());
     }
 }
